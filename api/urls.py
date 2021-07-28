7ad738ea544d30b51e7c49353b233c649227ed30
psick22 <kgirok@gmail.com>
2021-07-28 12:50:20 +0900

    create model (Movie, Rating)

---
+++ api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from django.contrib import admin
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
