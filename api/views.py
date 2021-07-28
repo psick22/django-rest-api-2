822b86174df823030230505decdbb06093ecded3
psick22 <kgirok@gmail.com>
2021-07-28 14:57:36 +0900

    some restrictions

--- api/views.py
+++ api/views.py
@@ -1,23 +1,33 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets, status
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from api.models import Movie, Rating
-from api.serializers import MovieSerializer, RatingSerializer
+from api.serializers import MovieSerializer, RatingSerializer, UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
     @action(detail=True, methods=['POST'])
     def rate_movie(self, request, pk=None):
         if 'stars' in request.data:
             movie = Movie.objects.get(id=pk)
             stars = request.data['stars']
-            # user = request.user
-            user = User.objects.get(id=1)
+            user = request.user
+
+            # user = User.objects.get(id=1)
             print(f"user : {user.username}")
 
             try:
@@ -37,12 +47,21 @@ class MovieViewSet(viewsets.ModelViewSet):
             response = {'message': 'you need to provide stars field'}
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
-    @action(detail=False, methods=['GET'])
-    def search_by_title(self, request, search_term):
-        print(request)
-        print(search_term)
+    # @action(detail=False, methods=['GET'])
+    # def search_by_title(self, request, search_term):
+    #     print(request)
+    #     print(search_term)
 
 
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
+    authentication_classes = (TokenAuthentication,)
+
+    def update(self, request, *args, **kwargs):
+        response = {'message': 'You cant update rating like that'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+    def create(self, request, *args, **kwargs):
+        response = {'message': 'You cant create rating like that'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
