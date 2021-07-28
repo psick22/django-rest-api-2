7d6149293d4ed2c44fd8245bfd9daccd4e382b43
psick22 <kgirok@gmail.com>
2021-07-28 14:20:19 +0900

    create custom method api with request data

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
-from django.contrib import admin
 from rest_framework import routers
+from api.views import MovieViewSet, RatingViewSet
 
 router = routers.DefaultRouter()
+router.register('movies', MovieViewSet)
+router.register('ratings', RatingViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- api/views.py
+++ api/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+from api.models import Movie, Rating
+from api.serializers import MovieSerializer, RatingSerializer
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+    @action(detail=True, methods=['POST'])
+    def rate_movie(self, request, pk=None):
+        if 'stars' in request.data:
+            movie = Movie.objects.get(id=pk)
+            stars = request.data['stars']
+            # user = request.user
+            user = User.objects.get(id=1)
+            print(f"user : {user.username}")
+
+            try:
+                rating = Rating.objects.get(user=user.id, movie=movie.id)
+                rating.stars = stars
+                rating.save()
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'Rating updated', 'result': serializer.data}
+
+            except:
+                rating = Rating.objects.create(user=user, movie=movie, stars=stars)
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'Rating created', 'result': serializer.data}
+
+            return Response(response, status=status.HTTP_200_OK)
+        else:
+            response = {'message': 'you need to provide stars field'}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, methods=['GET'])
+    def search_by_title(self, request, search_term):
+        print(request)
+        print(search_term)
+
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
